02cfb00ce4ae7d88360f5197f5deffbedd205348
Nich <116022893+Nich95@users.noreply.github.com>
2022-10-22 21:14:17 -0300

    Implementación del @viewchild (#5)

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
@@ -6,5 +6,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BusquedaComponent {
 
+  // dentro de los parentesis ponemos el elemento del html que se quiere buscar
+  // el "!" es un operador para asegurar que un elemento no es null
+  // entonces le decimos al txtBuscar que nunca va a ser nulo
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>; // le digo al elementref que es de tipo htmlinputelement
+                                                                    // y esto da las funciones para manupular los datos del input
 
+  buscar() {
+    const valor = this.txtBuscar.nativeElement.value;
+    console.log(valor);
+    this.txtBuscar.nativeElement.value = ""; // aqui le digo que el valor del txtBuscar es un string vacio
+  }
 }
