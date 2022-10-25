e802a05ca4fb62ad3c3df5a7ed108c236d627186
Nich <116022893+Nich95@users.noreply.github.com>
2022-10-25 16:51:06 -0300

    Desglose de la url (#14)

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { SearchGifsResponse, Gif } from '../interface/gitfs.interface';
@@ -10,6 +10,7 @@ export class GifsService {
 
   private _apiKey: string = 'btokDU8o5Kq1NVuthGjjGBAN2pZ4lPYG';
   private _historial: string[] = [];
+  private servicioUrl: string = 'https://api.giphy.com/v1/gifs';
 
   // todo: cambiar any por el tipo correspondiente
   public resultados: Gif[] = []; // es quien recibe la info de la petición
@@ -45,9 +46,14 @@ export class GifsService {
       localStorage.setItem( 'historial', JSON.stringify( this._historial ) );
     }
 
+    const params = new HttpParams() // objeto de angular
+                        .set( 'api_key', this._apiKey )
+                        .set( 'limit', '10' )
+                        .set( 'q', query ); 
+
     // se recomienda colocar aqui el tipo de datos que es la respuesta porque el get es generico
     // lectura: este get luce como la interface especificada en los <>
-    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=btokDU8o5Kq1NVuthGjjGBAN2pZ4lPYG&q=${ query }&limit=10`)
+    this.http.get<SearchGifsResponse>(`${ this.servicioUrl }/search`, { params } )
               .subscribe( ( resp ) => {
                 console.log( resp.data );
                 this.resultados = resp.data;
