5fed9f17fa11e65b0ab5a455182c1e49caa4da9e
nich-dev <nich.dev95@gmail.com>
2022-10-25 11:30:31 -0300

    Se da el tipo a los resultados

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,16 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import { SearchGifsResponse, Gif } from '../interface/gitfs.interface';
+
 @Injectable({
   providedIn: 'root' // esto le dice que es de acceso global para la aplicación
 })
 export class GifsService {
 
-  private  _apiKey: string = 'btokDU8o5Kq1NVuthGjjGBAN2pZ4lPYG';
+  private _apiKey: string = 'btokDU8o5Kq1NVuthGjjGBAN2pZ4lPYG';
   private _historial: string[] = [];
 
   // todo: cambiar any por el tipo correspondiente
-  public resultados: any[] = []; // es quien recibe la info de la petición
+  public resultados: Gif[] = []; // es quien recibe la info de la petición
 
   get historia() {
     return [...this._historial];
@@ -26,8 +28,10 @@ export class GifsService {
       this._historial = this._historial.splice(0, 10);
     }
 
-    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=btokDU8o5Kq1NVuthGjjGBAN2pZ4lPYG&q=${ query }&limit=10`)
-              .subscribe( ( resp: any ) => {
+    // se recomienda colocar aqui el tipo de datos que es la respuesta porque el get es generico
+    // lectura: este get luce como la interface especificada en los <>
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=btokDU8o5Kq1NVuthGjjGBAN2pZ4lPYG&q=${ query }&limit=10`)
+              .subscribe( ( resp ) => {
                 console.log( resp.data );
                 this.resultados = resp.data;
               });
